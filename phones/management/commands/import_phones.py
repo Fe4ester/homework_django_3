aae5772e4d369f39436b11327e6eab4b23692857
Fe4ester <xdxdxd4305@gmail.com>
2024-09-21 16:27:46 +0300

    last_change

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,17 +1,34 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
+from django.utils.text import slugify
 
 class Command(BaseCommand):
+    help = 'Импорт данных из CSV файла в модель Phone'
+
     def add_arguments(self, parser):
+        # Можешь добавить дополнительные аргументы команды, если потребуется
         pass
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open('phones.csv', 'r', encoding='utf-8') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            # Генерация slug из имени телефона
+            phone_slug = slugify(phone['name'])
+
+            # Создание или обновление объекта Phone
+            Phone.objects.update_or_create(
+                id=phone['id'],  # Используем id из файла CSV
+                defaults={
+                    'name': phone['name'],
+                    'image': phone['image'],
+                    'price': float(phone['price']),
+                    'release_date': phone['release_date'],
+                    'lte_exists': phone['lte_exists'].strip().lower() == 'true',
+                    'slug': phone_slug,
+                }
+            )
+
+        self.stdout.write(self.style.SUCCESS('Телефоны успешно импортированы'))
